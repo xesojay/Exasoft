e580b1b7ae6f083ba08d554f3b346b2a08ea1c06
xesojay <32509336+xesojay@users.noreply.github.com>
2023-12-31 21:41:53 +0000

    Add files via upload

---
+++ PalidromeApp1/PalidromeApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PalidromeApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> arrPalidrome = new List<string>();
+            string filePath = @"C:\\Users\Desktop\palindromes.txt";
+            string[] allLines = File.ReadAllLines(filePath);
+
+
+            foreach (string line in allLines)
+            {
+                var tempWord = line;
+
+                char[] charArray = line.ToCharArray();
+
+                Array.Reverse(charArray);
+
+                var reversedWord = new string(charArray);
+
+                if (tempWord.Equals(reversedWord))
+                {
+                    arrPalidrome.Add(tempWord);
+                }
+
+            }
+
+            for (int i = 0; i < arrPalidrome.Count; i++)
+            {
+                Console.WriteLine(arrPalidrome[i]);
+            }
+        }
+
+    }
+}
+
